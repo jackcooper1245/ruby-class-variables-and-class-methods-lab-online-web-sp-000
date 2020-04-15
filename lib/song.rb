1953b5a1577bc78de544a9c69d5835439d8b017e
Jack Cooper <jackcooper1245@gmail.com>
2020-04-15 13:29:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,7 @@ def self.genre_count
       if genre_count[genre]
         genre_count[genre] += 1 
       else
-        genre_count[genre] = []
+        genre_count[genre] = 1
       end
     end
     genre_count
